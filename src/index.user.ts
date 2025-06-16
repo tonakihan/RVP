4e7538551906e0d5c6e65a0f867ae62d6f4a6027
tonakihan <116086641+tonakihan@users.noreply.github.com>
2025-06-16 19:16:52 +0900

    Stash changes.
    Add observer

--- src/index.user.ts
+++ src/index.user.ts
@@ -14,15 +14,65 @@
 // ==/UserScript==
 "use strict";
 
+const config = {
+  /*
+    For ovserver. Count of run this script after launch. It script
+    will triggered when changed body of the HTML document.
+  */
+  countRun: 10,
+};
+//
+main().catch((e) => console.log(e));
+
 async function main() {
   console.info("Launch userscript RVP");
 
-  // Может быть множество...
-  // TODO: window.frames - если оно там, то удалить все элементы кроме видео (крч как обычно).
-  for (let video of document.getElementsByTagName("video")) {
-    let defaultPlayer = BuildinPlayer.findWrapperPlayer(video);
-    console.log("ОТЛАДКА:", defaultPlayer);
-  }
-  // Захватить видео.
+  console.debug("main: window:\n", window);
+
+  // TODO: сделать задержку (отложенное выполнение), что бы не грузить браузер
+  // Это нужно для хитрых - когда видео добавляется из скрипта.
+  let countRun = config.countRun || 10;
+  const observer = new MutationObserver(() => {
+    console.debug("observer is triggered");
+    replaceVideoPlayer();
+
+    countRun--;
+    if (countRun <= 0) {
+      console.debug("observer was disconnected");
+      observer.disconnect();
+    }
+  });
+
+  observer.observe(document.body, {
+    subtree: true,
+    childList: true,
+  });
+
+  /*
+    userscript, а именно tampermonkey запускает автоматом и в iframes.
+    Так, что реализация для iframes не требуется.
+  */
+  replaceVideoPlayer();
 }
-main().catch((e) => console.log(e));
+
+function replaceVideoPlayer() {
+  console.debug("replaceVideoPlayer: DOCUMENT.body:\n", document.body);
+  //console.debug("replaceVideoPlayer: HTML of body:\n", document.body.innerHTML);
+
+  for (let video of document.getElementsByTagName("video")) {
+    // TODO: if used stock in browser contol panel
+    let stockPlayer = BuildinPlayer.findWrapperPlayer(video);
+
+    // DEBUG
+    // For a visual accent, the video elements are handled.
+    //video.style.border = "10px solid CornflowerBlue";
+    console.debug(
+      "replaceVideoPlayer: foundPlayer:\n",
+      stockPlayer,
+      "\n video:\n",
+      video,
+    );
+
+    //TODO: Cut the stockPlayer and insert my player
+  }
+}
